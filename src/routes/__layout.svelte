b2c4d3b6518e5747dc17220d1039faf31903dc84
kvetoslavnovak <kvetoslav.novak@seznam.cz>
2022-07-26 14:38:19 +0200

    Initial commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { session } from '$app/stores';
+	import { supabaseClient } from '$lib/sb';
+	import { SupaAuthHelper } from '@supabase/auth-helpers-svelte';
+</script>
+
+<svelte:head>
+	<title>Email and Password Demo - Supabase Auth Helpers</title>
+</svelte:head>
+
+<SupaAuthHelper {supabaseClient} {session}>
+	<main>
+		<div>
+			<h2>
+				<a href="/">Supabase Auth Helpers Demo</a>
+			</h2>
+
+			<slot />
+
+			<div>
+				{#if $session?.user?.id}
+					<a href="/api/auth/logout">Sign out</a>
+				{/if}
+			</div>
+		</div>
+	</main>
+</SupaAuthHelper>
---
+++ src/routes/dashboard.svelte
@@ -0,0 +1,12 @@
+<script>
+	export let user;
+</script>
+
+<div>
+	<h3>This is protected route accesible only for ligged users</h3>
+	<p>Hello user {user.email}</p>
+</div>
+<div>
+	<p>User {user.email} details:</p>
+	<pre>{JSON.stringify(user, null, 2)}</pre>
+</div>
